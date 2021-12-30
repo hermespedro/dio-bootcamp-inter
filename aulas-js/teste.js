6b5ac1f034d5e548daa91b171e908d7b37547186
Hermes  Silva <hermes_mbc@hotmail.com>
2021-12-30 20:36:39 -0300

    finalizando primeiro modulo de JS

---
+++ aulas-js/teste.js
@@ -0,0 +1,24 @@
+// var a = 0;
+
+
+// function soma(a, b){
+//     console.log(a + b);
+//     console.log("Hermes pedro")
+//     return a + b;
+// }
+
+// soma(25, 30);
+
+// function returnArrayValue (array){
+//     let eventNums = [];
+//     for (var i = 0; i < array.length; i++){
+//             if(array[i] % 2 === 0){
+//                 eventNums.push(array[i]);
+//             } 
+//         }
+//     console.log(eventNums);
+// }
+
+// let array = [ 2 , 6 , 15 , 55];
+
+// returnArrayValue(array);
